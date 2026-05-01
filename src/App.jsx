6542487f2da0d4c2be50bf0d7f1fa46551e78c3d
Raghav Malhotra <yourName@student.unsw.edu.au>
2026-05-01 12:31:50 +1000

    Configuring files, successfully added shadcn components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { gameCatalog } from './data/gameCatalog.js';
-import HomePage from './pages/HomePage.jsx';
-import LeaderboardPage from './pages/LeaderboardPage.jsx';
-import GamePlaceholder from './pages/GamePlaceholder.jsx';
+import { Navigate, Route, Routes } from "react-router-dom";
+import { gameCatalog } from "./data/gameCatalog.js";
+import HomePage from "./pages/HomePage.jsx";
+import LeaderboardPage from "./pages/LeaderboardPage.jsx";
+import GamePlaceholder from "./pages/GamePlaceholder.jsx";
 
 function App() {
   return (
@@ -10,7 +10,11 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/leaderboard" element={<LeaderboardPage />} />
       {gameCatalog.map((game) => (
-        <Route key={game.id} path={game.route} element={<GamePlaceholder game={game} />} />
+        <Route
+          key={game.id}
+          path={game.route}
+          element={<GamePlaceholder game={game} />}
+        />
       ))}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
